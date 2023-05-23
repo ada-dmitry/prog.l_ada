096d8dea1a3be5ae9b15dfc1039374c79cb295d2
ada-dmitry <ada.dmitry@gmail.com>
2023-05-23 17:54:18 +0300

    maow

--- Homework/bintreeCalk/head.h
+++ Homework/bintreeCalk/head.h
@@ -8,3 +8,18 @@ typedef struct tree
     struct tree *left;
     struct tree *right;
 } Tree;
+
+Tree *tnew(void);
+Tree *leaf(char c);
+Tree *maketree(char expr[], int first, int last);
+unsigned char priority(char c);
+int lastop(char expr[], int first, int last);
+int calculate(Tree *root);
+void showtree(Tree *root, int level);
+Tree *create(unsigned char key);
+Tree *add(Tree *root, unsigned char key);
+Tree *search(Tree *root, unsigned char key);
+Tree *min(Tree *root);
+Tree *max(Tree *root);
+Tree *nextdel(Tree *root);
+void delete(Tree *del);
